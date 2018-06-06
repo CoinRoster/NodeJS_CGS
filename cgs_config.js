825a566e30eb3d218fc62dc4c915c1b16039eac7
gov <govi218mu@gmail.com>
2018-06-06 18:02:13 -0400

    testing forward flag

--- cgs_config.js
+++ cgs_config.js
@@ -60,7 +60,7 @@ exports.APIInfo={
 	"blockcypher":
 		{"token":"dea4a0d80c0a4567a758e2f6daa49050",
 		//or "btc/main", "btc/test3"
-		"network": bcypher_network,
+		"network": "test3",
 		//default miner fee in Satoshis (must be a BigNumber!)
 		"minerFee": new BigNumber("10"),
 		"storageMinerFee": new BigNumber("1000")}
