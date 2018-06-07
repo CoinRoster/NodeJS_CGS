1683187de8691c890002b9f5c40afb18b7d87439
gov <govi218mu@gmail.com>
2018-06-07 16:16:27 -0400

    cold storage bug

--- cgs.js
+++ cgs.js
@@ -551,7 +551,7 @@ function* RPC_pushToColdStorage(postData, requestObj, responseObj, batchResponse
 				bcapi.sendTX(data, (err, data) => {
 					if (err !== null || ((data["errors"] != null) && (data["errors"] != undefined) && (data["errors"] != ""))) {
 						var error = data["errors"].error;
-						trace("error posting the transaction: " + error);
+						trace("error posting the transaction: " + data);
 						replyError(postData, requestObj, responseObj, batchResponses, serverConfig.JSONRPC_EXTERNAL_API_ERROR, "There was a problem creating the transaction.", data);
 						return;
 					} else {
